f74866b494066d2df7c3b4a1f28b313440d52378
sastrapradana <bp846622@gmail.com>
2022-10-26 21:53:55 +0700

    mengubah navbar

---
+++ img/main.js
@@ -0,0 +1,9 @@
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('list');
+    navMenu.classList.toggle('hidden');
+})
+
